52c37cdbff302732c1434be0e29e2502733c616b
lucykorea414 <31093793+lucykorea414@users.noreply.github.com>
2022-07-12 17:34:59 +0900

    2-02

--- PyboTest/venvs/mysite/pybo/models.py
+++ PyboTest/venvs/mysite/pybo/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Question(models.Model):
+    subject = models.CharField(max_length=200)
+    content = models.TextField()
+    create_date = models.DateTimeField()
+
+    def __str__(self):
+        return self.subject
+
+class Answer(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    # ForeignKey 는 다른 모델과 연결하기 위해 사용
+    # on_delete=models.CASCADE 의 의미는 이 답변과 연결된 질문(Question)이 삭제될 경우 답변(Answer)도 함께 삭제된다는 의미이다.
+    content = models.TextField()
+    create_date = models.DateTimeField()
