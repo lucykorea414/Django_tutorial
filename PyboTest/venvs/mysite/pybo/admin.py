ffe9919b14c231944c656ca56686b362b220da39
lucykorea414 <31093793+lucykorea414@users.noreply.github.com>
2022-07-12 17:50:08 +0900

    2-03

--- PyboTest/venvs/mysite/pybo/admin.py
+++ PyboTest/venvs/mysite/pybo/admin.py
@@ -1,3 +1,11 @@
+import py_compile
 from django.contrib import admin
 
-# Register your models here.
+#admin / admin / admin@admin.com
+
+from .models import Question
+
+class QuestionAdmin(admin.ModelAdmin):
+    search_fields = ['subject']
+
+admin.site.register(Question, QuestionAdmin)
