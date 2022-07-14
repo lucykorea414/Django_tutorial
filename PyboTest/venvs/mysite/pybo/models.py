ba46d898b2a04da08e1e4e152132bfe3c4c451ae
lucykorea414 <31093793+lucykorea414@users.noreply.github.com>
2022-07-14 17:40:37 +0900

    3-09

--- PyboTest/venvs/mysite/pybo/models.py
+++ PyboTest/venvs/mysite/pybo/models.py
@@ -6,6 +6,7 @@ class Question(models.Model):
     subject = models.CharField(max_length=200)
     content = models.TextField()
     create_date = models.DateTimeField()
+    modify_date = models.DateTimeField(null=True, blank=True)
 
     def __str__(self):
         return self.subject
@@ -17,3 +18,4 @@ class Answer(models.Model):
     # on_delete=models.CASCADE 의 의미는 이 답변과 연결된 질문(Question)이 삭제될 경우 답변(Answer)도 함께 삭제된다는 의미이다.
     content = models.TextField()
     create_date = models.DateTimeField()
+    modify_date = models.DateTimeField(null=True, blank=True)
